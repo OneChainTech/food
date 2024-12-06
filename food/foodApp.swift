3ac590176626e6118800d1ec40bd5f47287110d8
OneChainTech <zhenghong596gm@gmail.com>
2024-12-06 13:14:33 +0800

    init

---
+++ food/foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodApp.swift
+//  food
+//
+//  Created by zhenghong on 2024/12/6.
+//
+
+import SwiftUI
+
+@main
+struct foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
